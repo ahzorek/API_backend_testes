8b2424df784cbed8f3509f52fa20d171557adf8c
ahzorek <andrezorek@iMac-de-Andre.local>
2023-09-30 01:06:37 -0300

    mais fontes adicionadas. tentando manter resposta em cache por mais tempo, vejamos. noticias sao entregues de forma randomica.

--- functions/newsApiv2.js
+++ functions/newsApiv2.js
@@ -2,46 +2,58 @@ require('dotenv').config()
 const cheerio = require('cheerio');
 
 const headers = {
-  'Cache-Control': 'public, s-maxage=3600',
+  'Cache-Control': 'public, s-maxage=7200',
   'Content-Type': 'application/json; charset=utf-8',
   'Access-Control-Allow-Origin': '*'
 }
 exports.handler = async (event, context) => {
-  const globo_Headlines = await getGloboHeadline()
-  const uolMaisLidas = await getUOLMaisLidas()
-
-  const headlines = [
-    ...globo_Headlines,
-    ...uolMaisLidas
-  ]
-
-
   if (event.httpMethod !== 'GET') {
     return {
       statusCode: 405,
       body: JSON.stringify({ error: 'Método não permitido', message: 'Este endpoint só suporta solicitações GET.' }),
       headers
-    };
-  } else return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify({ headlines })
+    }
+  } else {
+    const globoComTopHeadlines = await getGloboComTopHeadlines()
+    const uolMaisLidas = await getUOLMaisLidas()
+    const brasil247 = await getBrasil247()
+    const cnnBrasilTecnologia = await getCNNBrasil()
+    const cnnBrasilNoticias = await getCNNBrasil('politica')
+    const bbcBrasil = await getBBCBrasil()
+
+
+    let AGGNEWS = [
+      ...globoComTopHeadlines,
+      ...cnnBrasilNoticias,
+      ...brasil247,
+      ...bbcBrasil,
+      ...uolMaisLidas,
+      ...cnnBrasilTecnologia
+    ]
+
+    const headlines = AGGNEWS.sort(() => Math.random() - 0.5)
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ headlines })
+    }
   }
 }
 
-async function getGloboHeadline() {
+async function getGloboComTopHeadlines() {
   const res = await fetch('https://globo.com')
   const html = await res.text()
   const $ = cheerio.load(html)
   const topGloboCom = $('.topglobocom__ranking__content-item')
-  let headlines = []
+  let content = []
 
   topGloboCom.each((index, element) => {
     const title = $(element).text()
     const href = $(element).find('.post__link').attr('href')
-    headlines.push({ title, href, source: 'Globo.com' })
+    content.push({ title, href, source: 'Globo.com' })
   })
-  return headlines
+  return content
 }
 
 async function getUOLMaisLidas() {
@@ -49,19 +61,78 @@ async function getUOLMaisLidas() {
   const html = await res.text()
   const $ = cheerio.load(html)
   const mais_lidas_uol = $('.mostRead__item')
-  let maislidas = []
+  let content = []
 
   mais_lidas_uol.each((index, element) => {
     const rawTitle = $(element).find('h3').text()
     const title = rawTitle.trim()
-    // console.log('ISSO É UM TITULO', title);
     const href = $(element).find('a').attr('href')
-    maislidas.push({
+    content.push({
       title,
       href,
       source: 'UOL'
     })
   })
-  return maislidas
+  return content
 }
 
+async function getBrasil247() {
+  const res = await fetch('https://www.brasil247.com/ultimas-noticias')
+  const html = await res.text()
+  const $ = cheerio.load(html)
+  const target = $('.articleGrid__item')
+  let content = []
+
+  target.each((index, element) => {
+    const title = $(element).find('h3').text()
+    // console.log('ISSO DEVE SER UM TITULO', title);
+    const href = $(element).find('h3').find('a').attr('href')
+    // console.log('ISSO DEVE SER UM HREF', href);
+    content.push({
+      title,
+      href,
+      source: 'Brasil247'
+    })
+  })
+  content = content.slice(1)
+  return content
+}
+
+async function getCNNBrasil(section = 'tecnologia') {
+  const res = await fetch(`https://www.cnnbrasil.com.br/${section}/`)
+  const html = await res.text()
+  const $ = cheerio.load(html)
+  const target = $('.home__list__item')
+  const h2title = $('.home__category').text()
+  let content = []
+
+  target.each((index, element) => {
+    const title = $(element).find('a').find('h3').text()
+    const href = $(element).find('a').attr('href')
+    content.push({
+      title,
+      href,
+      source: `CNN Brasil - ${h2title}`
+    })
+  })
+  return content
+}
+
+async function getBBCBrasil() {
+  const res = await fetch(`https://www.bbc.com/portuguese/topics/cz74k717pw5t`)
+  const html = await res.text()
+  const $ = cheerio.load(html)
+  const target = $('[data-testid="topic-promos"]').find('li')
+  let content = []
+
+  target.each((index, element) => {
+    const title = $(element).find('h2').text()
+    const href = $(element).find('h2').find('a').attr('href')
+    content.push({
+      title,
+      href,
+      source: `BBC Brasil`
+    })
+  })
+  return content
+}
